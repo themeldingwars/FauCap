5b48e9b0e2289b68c12b944d9c4814c1547be3ca
freakbyte <freakbyte@gmail.com>
2021-04-04 22:51:22 +0200

    Added Test project for testing the new parsing features

---
+++ Test/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using FauCap;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string file = @"sadface.faucap"; // path to some faucap
+
+            Stopwatch sw = Stopwatch.StartNew();
+            
+            List<GameSession> sessions = GameSession.Read(file, false);
+            
+            Console.WriteLine("Parse time: " + sw.ElapsedMilliseconds + "ms");
+            
+
+            int x = 0;
+            foreach (GameSession session in sessions)
+            {
+                Console.WriteLine("Session: " + x);
+                Console.WriteLine("Dgrm: " + session.Datagrams.Count);
+                Console.WriteLine("Pckt: " + session.Packets.Count);
+                Console.WriteLine("Msgs: " + session.Messages.Count);
+                Console.WriteLine("------------------------");
+                x++;
+            }
+            
+        }
+    }
+}
